04d5f4b3b58f18f3f1d1a7624ece0c1e7a4983f9
Popodil <popodil@bornready.eu>
2023-12-15 00:06:35 +0100

    Added eureka server functionality with ocelot

---
+++ Gateway/Startup.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.OpenApi.Models;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Eureka;
+using Steeltoe.Discovery.Client;
+using Steeltoe.Discovery.Eureka;
+
+namespace Gateway
+{
+    public class Startup
+    {
+        private IWebHostEnvironment _env;
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            _env = env;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.SetIsOriginAllowed((host) => true)
+                        //.WithOrigins("http://localhost:19006/")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                });
+            });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestAPI", Version = "v1" });
+            });
+
+            services.AddOcelot()
+                .AddEureka();
+            services.AddDiscoveryClient();
+            services.AddServiceDiscovery(options => options.UseEureka());
+
+
+            //string connectionstring;
+            //if (_env.IsDevelopment()) connectionstring = Configuration.GetConnectionString("DevConnection");
+            //else connectionstring = Configuration.GetConnectionString("DefaultConnection");
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RestAPI v1"));
+
+            app.UseRouting();
+
+            app.UseCors();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseDiscoveryClient();
+            app.UseOcelot().Wait();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
